43e02e599e0f21219d12dbeb7aff3f485b4538a9
unknown <kinekarpallavi21@gmail.com>
2022-06-03 17:01:47 +0530

    Add:Calculate daily wage of employee if it is present

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -7,17 +7,22 @@ class EmployeeWages{
    }
    //create function to check employee attendance using rand()
    function empCheck(){
+    $WagePerHour = 20;
+    $fullDayHour = 8;
+    $dailyWage =0;
     $ispresent = 1;
     $check =rand(0,1);
     if( $check==$ispresent){
-        echo "Employee is present";
+        echo "Employee is present"." ";
+        $dailyWage = $WagePerHour * $fullDayHour;
     }
     else{
-        echo "Employee is Absent";
+        echo "Employee is absent";
     }
+    echo " Its daily wage is :". $dailyWage;
 }
 }
 EmployeeWages::welcomeMessage();//call static function through class
 $emp=new EmployeeWages();//create object of class
-$emp->empCheck();//call a method to check employee is present or absent
+$emp->empCheck();//call a method to calculate daily employee wage if it is present 
 ?>
