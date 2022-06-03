9c003c4b9f4ded14ab9212df362ec20396da1620
unknown <kinekarpallavi21@gmail.com>
2022-06-03 19:37:27 +0530

    Add:calculate wage till the condition total working hours and days is reached

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -5,6 +5,7 @@ class EmployeeWages{
     const FULL_DAY_HOUR = 8;
     const EMP_WAGE_PER_HOUR = 20;
     const WORKING_DAYS = 20;
+    const MAX_HOURS = 100;
     const FULL_TIME = 1;
     const PART_TIME = 2;
     //create static function to display welcome message
@@ -15,7 +16,10 @@ class EmployeeWages{
    function empCheck(){
     $dailyHour = 0;
     //UC5-use for loop to till that working days
-    for($day=0;$day<= EmployeeWages::WORKING_DAYS;$day++){
+   // for($day=0;$day<= EmployeeWages::WORKING_DAYS;$day++){
+   //UC6-Use while to check number of working days and maximmum working hours
+    $day = 0;
+    while($day< EmployeeWages::WORKING_DAYS && $dailyHour <= EmployeeWages::MAX_HOURS){
     $check =rand(0,2);
     switch($check){
     case EmployeeWages::FULL_TIME:
@@ -31,9 +35,12 @@ class EmployeeWages{
         echo "Employee is absent"." \n";
         break;
     }
+    $day++;
 }
     $monthlyWage=EmployeeWages::EMP_WAGE_PER_HOUR* $dailyHour;
-    echo " Its monthly wage is :".  $monthlyWage."\n";
+    echo " Its monthly wage is:". $monthlyWage."\n";
+    echo " Number of  total working days: $day \n";
+    echo " Number of working hours: $dailyHour";
    }
 }
 EmployeeWages::welcomeMessage();//call static function through class
