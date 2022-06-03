51d4be3d1ac806b7192d684bfb2295ae2869e8c0
unknown <kinekarpallavi21@gmail.com>
2022-06-03 19:21:17 +0530

    Add:take for loop to reach no.of working days and calculate monthly wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -4,6 +4,7 @@ class EmployeeWages{
     const PART_TIME_HOUR = 4;
     const FULL_DAY_HOUR = 8;
     const EMP_WAGE_PER_HOUR = 20;
+    const WORKING_DAYS = 20;
     const FULL_TIME = 1;
     const PART_TIME = 2;
     //create static function to display welcome message
@@ -13,23 +14,26 @@ class EmployeeWages{
    //create function to check employee attendance using rand()
    function empCheck(){
     $dailyHour = 0;
+    //UC5-use for loop to till that working days
+    for($day=0;$day<= EmployeeWages::WORKING_DAYS;$day++){
     $check =rand(0,2);
     switch($check){
     case EmployeeWages::FULL_TIME:
-        echo "Employee is present"." ";
-        $dailyHour = EmployeeWages::FULL_DAY_HOUR;
+        echo "Employee is present"." \n";
+        $dailyHour += EmployeeWages::FULL_DAY_HOUR;
         break;
     
     case EmployeeWages::PART_TIME:
-        echo "Employee is present half day"." ";
-        $dailyHour = EmployeeWages::PART_TIME_HOUR;  
+        echo "Employee is present half day"." \n";
+        $dailyHour += EmployeeWages::PART_TIME_HOUR;  
         break;
     default:
-        echo "Employee is absent";
+        echo "Employee is absent"." \n";
         break;
     }
-    $dailyWage=EmployeeWages::EMP_WAGE_PER_HOUR* $dailyHour;
-    echo " Its daily wage is :". $dailyWage;
+}
+    $monthlyWage=EmployeeWages::EMP_WAGE_PER_HOUR* $dailyHour;
+    echo " Its monthly wage is :".  $monthlyWage."\n";
    }
 }
 EmployeeWages::welcomeMessage();//call static function through class
