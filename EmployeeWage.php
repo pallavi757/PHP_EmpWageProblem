ebd0b0c45fc839e1d8c633f7aae387e37a9fcb51
unknown <kinekarpallavi21@gmail.com>
2022-06-03 17:47:15 +0530

    Add:Refactored UC2 add Part time employee and calculate daily wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,24 +1,32 @@
 <?php
 //create class
 class EmployeeWages{
+    const PART_TIME_HOUR = 4;
+    const FULL_DAY_HOUR = 8;
+    const EMP_WAGE_PER_HOUR = 20;
+    const FULL_TIME = 1;
+    const PART_TIME = 2;
     //create static function to display welcome message
    static function welcomeMessage(){
        echo "....Welcome to employee wage problem...\n";
    }
    //create function to check employee attendance using rand()
    function empCheck(){
-    $WagePerHour = 20;
-    $fullDayHour = 8;
-    $dailyWage =0;
-    $ispresent = 1;
-    $check =rand(0,1);
-    if( $check==$ispresent){
+    $dailyHour = 0;
+    $check =rand(0,2);
+    if( $check==EmployeeWages::FULL_TIME){
         echo "Employee is present"." ";
-        $dailyWage = $WagePerHour * $fullDayHour;
+        $dailyHour = EmployeeWages::FULL_DAY_HOUR;
+    }
+    elseif($check==EmployeeWages::PART_TIME){
+        echo "Employee is present half day"." ";
+        $dailyHour = EmployeeWages::PART_TIME_HOUR;  
+
     }
     else{
         echo "Employee is absent";
     }
+    $dailyWage=EmployeeWages::EMP_WAGE_PER_HOUR* $dailyHour;
     echo " Its daily wage is :". $dailyWage;
 }
 }
