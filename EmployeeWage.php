48d73255029586eeb3594af82330a953aefd9f2b
unknown <kinekarpallavi21@gmail.com>
2022-06-03 15:28:23 +0530

    Add:Display Welcome Message

---
+++ EmployeeWage.php
@@ -0,0 +1,10 @@
+<?php
+//create class
+class EmployeeWages{
+    //create static function to display welcome message
+   static function welcomeMessage(){
+       echo "....Welcome to employee wage problem...\n";
+   }
+}
+EmployeeWages::welcomeMessage();//call static function through class
+?>
