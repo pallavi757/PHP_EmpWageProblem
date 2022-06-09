90af61c83260aad63401df198fb14c3ee18b31a5
unknown <kinekarpallavi21@gmail.com>
2022-06-09 09:49:32 +0530

    Add:Using instance varible instead of paramerized function to save total wages for each company

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,13 +1,36 @@
 <?php
 //create class
 class EmployeeWages{
-    const PART_TIME_HOUR = 4;
-    const FULL_DAY_HOUR = 8;
-   // const EMP_WAGE_PER_HOUR = 20;
-   // const WORKING_DAYS = 20;
-   // const MAX_HOURS = 100;
-    const FULL_TIME = 1;
-    const PART_TIME = 2;
+    public  $PART_TIME_HOUR = 4;
+    public  $FULL_DAY_HOUR = 8;
+    public $EMP_WAGE_PER_HOUR = 20;
+    public $WORKING_DAYS = 20;
+    public $MAX_HOURS = 100;
+    public $FULL_TIME = 1;
+    public  $PART_TIME = 2;
+
+    private $workingDaysPerMonth;
+    private $workingHoursPerMonth;
+    private $wagePerHrs;
+    function __construct($workingDaysPerMonth, $workingHoursPerMonth, $wagePerHrs)
+    {
+        $this->workingDaysPerMonth = $workingDaysPerMonth;
+        $this->workingHoursPerMonth = $workingHoursPerMonth;
+        $this->wagePerHrs = $wagePerHrs;
+    }
+    function get_workingDaysPerMonth()
+    {
+        return $this->workingDaysPerMonth;
+    }
+    function get_workingHoursPerMonth()
+    {
+        return $this->workingHoursPerMonth;
+    }
+    function get_wagePerHrs()
+    {
+        return $this->wagePerHrs;
+    }
+    
     //create static function to display welcome message
    static function welcomeMessage(){
        echo "....Welcome to employee wage problem...\n";
@@ -22,14 +45,14 @@ class EmployeeWages{
     while($day< $WORKING_DAYS_PER_MONTH && $dailyHour <= $WORKING_HOURS_PER_MONTH){
     $check =rand(0,2);
     switch($check){
-    case EmployeeWages::FULL_TIME:
+    case $this->FULL_TIME:
         echo "Employee is present"." \n";
-        $dailyHour += EmployeeWages::FULL_DAY_HOUR;
+        $dailyHour += $this->FULL_DAY_HOUR;
         break;
     
-    case EmployeeWages::PART_TIME:
+    case $this->PART_TIME:
         echo "Employee is present half day"." \n";
-        $dailyHour += EmployeeWages::PART_TIME_HOUR;  
+        $dailyHour += $this->PART_TIME_HOUR;  
         break;
     default:
         echo "Employee is absent"." \n";
@@ -38,25 +61,40 @@ class EmployeeWages{
     $day++;
 }
     $monthlyWage=$WAGE_PER_HR* $dailyHour;
-    echo " Its monthly wage is:". $monthlyWage."\n";
-    echo " Number of  total working days: $day \n";
-    echo " Number of working hours: $dailyHour";
+    echo "Its monthly wage is:". $monthlyWage."\n";
+    echo "Number of  total working days: $day \n";
+    echo "Number of working hours:" . $dailyHour;
    }
-   //UC8-Compute wages for multiple companies
+  
    function addCompany(){
-    $companyName = readline('Enter Name of Company: ');
+        $companyName = readline('Enter Name of Company: '.'\n');
         echo "Employee Wage Compute For:".   $companyName."\n";
         $WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
         $WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
         $WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
-        $this->empCheck($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);/* pass working days,working hrs for month and wage per hrs to calculate monthly wages*/
+       // $this->empCheck($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);/* pass working days,working hrs for month and wage per hrs to calculate monthly wages*/
+       return new  EmployeeWages($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR); 
 
    }
 }
- EmployeeWages::welcomeMessage();//call static function through class
-$companyObj1=new EmployeeWages();//create object for calculate wages for first company 
-echo $companyObj1->addCompany();
+EmployeeWages::welcomeMessage();//call static function through class
+
+echo "Enter details to comupte monthly wages for first company \n";
+$companyName = readline('Enter Name of Company: ');        
+echo "Employee Wage Compute For:".   $companyName."\n";
+$WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
+$WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
+$WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
+$companyObj1 = new EmployeeWages($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);
+$companyObj1->empCheck($companyObj1->get_workingDaysPerMonth(),$companyObj1->get_workingHoursPerMonth(),$companyObj1->get_wagePerHrs());
 echo "\n";
-$companyObj2=new EmployeeWages();//create object for calculate wages for second company
-echo $companyObj2->addCompany();
+
+echo "Enter details to comupte monthly wages for second company \n";
+$companyName = readline('Enter Name of Company: ');        
+echo "Employee Wage Compute For:".   $companyName."\n";
+$WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
+$WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
+$WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
+$companyObj2 = new EmployeeWages($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);
+$companyObj2->empCheck($companyObj2->get_workingDaysPerMonth(),$companyObj2->get_workingHoursPerMonth(),$companyObj2->get_wagePerHrs());
 ?>
