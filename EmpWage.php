b436dbc2b3853715a2d1d3440b7794bcc7329134
unknown <kinekarpallavi21@gmail.com>
2022-06-10 22:44:58 +0530

    Add:Check employee attendance using rand

--- EmpWage.php
+++ EmpWage.php
@@ -5,6 +5,19 @@ class EmployeeWages{
    static function welcomeMessage(){
        echo "....Welcome to employee wage problem...\n";
    }
+   //Function to Check Employee is Present or Absent
+   function empCheckAttendence() {
+    $empCheck = rand(0,1);
+    if($empCheck == 1) {
+        echo "Employee is Present";
+    }
+    else {
+        echo "Employee is absent";
+    }
+
+}
 }
 EmployeeWages::welcomeMessage();
+$emp = new EmployeeWages();
+$emp->empCheckAttendence();
 ?>
