5bf44b0a29944ee307905710f14a992cf97bf17a
unknown <kinekarpallavi21@gmail.com>
2022-06-10 23:57:33 +0530

    Add:Refactor UC4 Using switch case and calculate employee daily wage

--- EmpWage.php
+++ EmpWage.php
@@ -3,6 +3,12 @@ class EmployeeWages{
     const wagePerHr = 20;
     const fullDayHr = 8;
     const partTimeHr = 4;
+
+    //create static function to display welcome message
+   static function welcomeMessage(){
+    echo "....Welcome to employee wage problem...\n";
+}
+
 /* function to compute daily 
 wage of employee*/
 static function dailyWage($wagePerHr,$hour) {
@@ -10,25 +16,26 @@ static function dailyWage($wagePerHr,$hour) {
     return $dailyWage;
 }
 
-static function empCheckAttendance() {
-    $empCheck = rand(0,2);
-    if($empCheck == 1) {
-        echo "Employee is present full time" ."\n";
-        $dailyWage= EmployeeWages::dailyWage(EmployeeWages::wagePerHr,EmployeeWages::fullDayHr);
-        echo"Daily wage of full time employee is:".$dailyWage;
+ function empCheckAttendance() {
+    $empCheck = rand(0, 2);
+    switch ($empCheck) {
+        case 1:
+            echo "Employee is present full time"."\n";
+            $dailyWage = EmployeeWages::dailyWage(EmployeeWages::wagePerHr,EmployeeWages::fullDayHr);
+            echo "Daily Wage of Full time Employee:" . $dailyWage;
+            break;
+        case 2:
+            echo "Employee is half day present"  ."\n";
+            $partTimeWage = EmployeeWages::dailyWage(EmployeeWages::wagePerHr,EmployeeWages::partTimeHr);
+            echo "Daily Wage of Part time Presnt:" . $partTimeWage;
+            break;
+        default:
+            echo "Employee is Absent";
     }
-    elseif($empCheck == 2) {
-        echo "Employee is half day present" ."\n";
-        $PartWage = EmployeeWages::dailyWage(EmployeeWages::wagePerHr,EmployeeWages::partTimeHr);
-        echo "Daily Wage of Part time employee:".$PartWage;
-    }
-    else {
-        echo "Employee is Absent";
-    }
-
-   }
+}
 }
 
-//EmployeeWages::welcomeMessage();
-EmployeeWages::empCheckAttendance();
+EmployeeWages::welcomeMessage();
+$emp = new EmployeeWages();
+$emp ->empCheckAttendance();
 ?>
