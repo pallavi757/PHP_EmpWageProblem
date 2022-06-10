4d0e3e906e2ed2e80e8a13b49a92e9b502d0d07d
unknown <kinekarpallavi21@gmail.com>
2022-06-11 00:30:05 +0530

    Add:Refactor UC7 use class and methods

--- EmpWage.php
+++ EmpWage.php
@@ -1,5 +1,11 @@
 <?php
-class EmployeeWages{
+
+/*
+  Employee Wage Program to find Employee Present or Not and Calculating Wages for a Month
+  total working hours Using Class and Methods
+*/
+class EmployeeWages
+{
     const wagePerHr = 20;
     const fullDayHr = 8;
     const partTimeHr = 4;
@@ -8,23 +14,24 @@ class EmployeeWages{
     const isPartTime = 2;
     const isAbsent = 0;
     const maxHrs = 100;
-    public $wrkhrs =0;
-
+    public $wrkhrs = 0;
 
     //create static function to display welcome message
-   static function welcomeMessage(){
-    echo "....Welcome to employee wage problem...\n";
-}
+    static function welcomeMessage()
+    {
+        echo "....Welcome to employee wage problem...\n";
+    }
 
- function empCheckAttendance() {
-    $empCheck = rand(0, 2);
+    function empCheckAttendance()
+    {
+        $empCheck = rand(0, 2);
         switch ($empCheck) {
-            case EmployeeWages::isFullTime :
+            case EmployeeWages::isFullTime:
                 echo "Employee is present full time" . "\n";
                 return EmployeeWages::fullDayHr;
                 break;
-            
-            case EmployeeWages::isPartTime : 
+
+            case EmployeeWages::isPartTime:
                 echo "Employee is half day present" . "\n";
                 return EmployeeWages::partTimeHr;
                 break;
@@ -37,32 +44,33 @@ class EmployeeWages{
 
     /* function to compute daily 
 wage of employee*/
- function dailyWage() {
-    $this->wrkhrs = $this->empCheckAttendance();
-    $dailyWage = EmployeeWages::wagePerHr * $this->wrkhrs;
-    echo "Total hours:".$this->wrkhrs ."\n";
-    echo "Daily Wage:".$dailyWage ."\n";
-    return $dailyWage;
-}
-
-function calculateMonthlyWage() {
-    $monthlyWage = 0;
-    $day = 0;
-    $totalWorkingHrs = 0;
-    while($day<EmployeeWages::workDaysPerMonth && $totalWorkingHrs <=EmployeeWages::maxHrs) {
-        $dailyWage = $this->dailyWage();
-        $monthlyWage+=$dailyWage;
-        $totalWorkingHrs+=$this->wrkhrs;
-        $day++;
+    function dailyWage()
+    {
+        $this->wrkhrs = $this->empCheckAttendance();
+        $dailyWage = EmployeeWages::wagePerHr * $this->wrkhrs;
+        echo "Total hours:" . $this->wrkhrs . "\n";
+        echo "Daily Wage:" . $dailyWage . "\n";
+        return $dailyWage;
     }
-    echo "Total Working Hours:".$totalWorkingHrs ."\n";
-    echo "Total Monthly Wage:".$monthlyWage;
-
-}
+  /* function to compute monthly 
+wage of employee*/
+    function calculateMonthlyWage()
+    {
+        $monthlyWage = 0;
+        $day = 0;
+        $totalWorkingHrs = 0;
+        while ($day < EmployeeWages::workDaysPerMonth && $totalWorkingHrs <= EmployeeWages::maxHrs) {
+            $dailyWage = $this->dailyWage();
+            $monthlyWage += $dailyWage;
+            $totalWorkingHrs += $this->wrkhrs;
+            $day++;
+        }
+        echo "Total Working Hours:" . $totalWorkingHrs . "\n";
+        echo "Total Monthly Wage:" . $monthlyWage;
+    }
 }
 
 EmployeeWages::welcomeMessage();
 $emp = new EmployeeWages();
 $emp->calculateMonthlyWage();
-//$emp ->empCheckAttendance();
 ?>
