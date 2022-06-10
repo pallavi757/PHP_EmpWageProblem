258e61d68da0e7f2f7d498d782688e554b62d1ee
unknown <kinekarpallavi21@gmail.com>
2022-06-10 22:31:46 +0530

    Add:Print welcome message

---
+++ EmpWage.php
@@ -0,0 +1,10 @@
+<?php
+class EmployeeWages{
+
+ //create static function to display welcome message
+   static function welcomeMessage(){
+       echo "....Welcome to employee wage problem...\n";
+   }
+}
+EmployeeWages::welcomeMessage();
+?>
