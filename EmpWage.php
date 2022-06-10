005a36a628db7d9fa02ccbba654cef09d7797bbd
unknown <kinekarpallavi21@gmail.com>
2022-06-10 23:18:34 +0530

    Add:Refactor UC2 Check employee attendance and calculate daily wage og employee

--- EmpWage.php
+++ EmpWage.php
@@ -1,23 +1,29 @@
 <?php
 class EmployeeWages{
-
+    const wagePerHr = 20;
+    const fullDayHr = 8;
  //create static function to display welcome message
    static function welcomeMessage(){
        echo "....Welcome to employee wage problem...\n";
    }
-   //Function to Check Employee is Present or Absent
-   function empCheckAttendence() {
-    $empCheck = rand(0,1);
-    if($empCheck == 1) {
-        echo "Employee is Present";
-    }
-    else {
-        echo "Employee is absent";
-    }
 
+/* function to compute daily 
+wage of employee*/
+ function dailyWage() {
+        $empCheck = rand(0,1);
+        if($empCheck == 1) {
+            echo " Employee is Present" ."\n";
+            $dailyWage = EmployeeWages::wagePerHr * EmployeeWages::fullDayHr;
+            echo " Daily Wage of Employee:".$dailyWage;
+        }
+        else {
+            echo "Employee is absent";
+        }
+
+    }
 }
-}
+
 EmployeeWages::welcomeMessage();
 $emp = new EmployeeWages();
-$emp->empCheckAttendence();
+$emp->dailyWage();
 ?>
